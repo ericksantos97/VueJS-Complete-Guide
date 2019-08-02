c16a5289d0781cfa34f9b40a8c4d0c4fffb75738
Erick Alessandro dos Santos <erick.santos@castgroup.com.br>
2019-08-02 14:15:37 -0300

    Animacoes, diretivas, filtros

---
+++ filtro-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ usuarioLogado }}</p>
+		<p>{{ cpf | formatarCpf }}</p>
+		<p>{{ 14955163858 | formatarCpf | inverter }}</p>
+		<input type="text" :value="cpf | formatarCpf">
+		<hr>
+		<Frutas />
+		<hr>
+		<div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta">
+                {{ fruta }}
+             </li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    	</div>
+	</div>
+</template>
+
+<script>
+import Frutas from './Frutas.vue'
+import frutasMixin from './frutasMixin'
+import usuarioMixin from './usuarioMixin'
+
+export default {
+	components: { Frutas },
+	mixins: [ frutasMixin, usuarioMixin ],
+	filters: {
+		formatarCpf(valor) {
+			const arr = `${valor}`.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
+	data() {
+		return {
+			cpf: '45126637895',
+			// O que esta dentro do data do Componente tem prioridade ao Mixin, e sera substituido
+			// frutas: ['abacate']
+		}
+	},
+	created() {
+        console.log('Created - App Vue!')
+    }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input {
+	font-size: 2.5rem;
+}
+</style>
---
+++ filtro-mixins/filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4>{{ global }}</h4>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta">
+                {{ fruta }}
+             </li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
+</template>
+
+<script>
+import frutasMixin from './frutasMixin'
+import usuarioMixin from './usuarioMixin'
+
+export default {
+   mixins: [ frutasMixin, usuarioMixin ],
+   created() {
+        console.log('Created - FrutasVue!')
+    },
+}
+</script>
+
+<style>
+
+</style>
