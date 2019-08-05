f3270d0e4cf1fbb8b8f4c0dcf3a566bb0b01a812
Erick Santos <erick.tec97@gmail.com>
2019-08-05 18:41:47 -0300

    axios crud

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens"
+			:key="mensagem.texto" :variant="mensagem.tipo">{{ mensagem.texto }}</b-alert>
 		<b-card>
 			<b-form-group label="Nome:">
 				<b-form-input type="text" size="lg" 
@@ -23,6 +25,8 @@
 				<strong>Nome: </strong> {{ usuario.nome }}<br/>
 				<strong>Email: </strong> {{ usuario.email }}<br/>
 				<strong>ID: </strong> {{ id }}
+				<b-button variant="warning" size="lg" @click="carregar(id)">Carregar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2" @click="excluir(id)">Excluir</b-button>
 			</b-list-group-item>
 		</b-list-group>
 	</div>
@@ -32,7 +36,9 @@
 export default {
 	data() {
 		return {
+			mensagens: [],
 			usuarios: [],
+			id: null,
 			usuario: {
 				nome: '',
 				email: ''
@@ -40,12 +46,36 @@ export default {
 		}
 	},
 	methods: {
-		salvar() {
-			this.$http.post('usuarios.json', this.usuario)
-				.then(resp => {
-					this.usuario.nome = ''
-					this.usuario.email = ''
+		limpar() {
+			this.usuario.nome = '',
+			this.usuario.email = '',
+			this.id = null,
+			this.mensagens = []
+		},
+		carregar(id) {
+			this.id = id,
+			this.usuario = { ...this.usuarios[id] }
+		},
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}.json`).then(() => this.limpar())
+				.catch(_ => {
+					this.limpar()
+					this.mensagens.push({
+						texto: 'Ocorreu um erro na exclusão',
+						tipo: 'danger'
+					})
 				})
+		},
+		salvar() {
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario).then(_ => {
+				this.limpar()
+				this.mensagens.push({
+					texto: 'Operação realizada com sucesso!',
+					tipo: 'success'
+				})
+			})
 		},
 		obterUsuarios() {
 			this.$http('usuarios.json').then(resp => {
